20d458954fd5a5f0cb6927361cd05a8dae706c8b
saulofsantos-crypto <saulo.f.santos@ba.estudante.senai.br>
2026-04-14 12:32:53 -0300

    Add files via upload

---
+++ testes.cy.js
@@ -0,0 +1,37 @@
+// ----------------- TESTE 1: ABRINDO A PAGINA E VERIFICANDO SE O BODY APARECE------------------
+describe('teste pagina inicial', () => {
+  it('deve abrir a pagina e verificar se o body esta aparecendo', () => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+    cy.get('body').should('be.visible');
+  });
+});
+
+// ----------------- TESTE 2: TESTANDO O COMPORTAMENTO DO BOTÃO COM O CLICK ---------------------
+describe('teste botão de busca', () => {
+  it('deve clicar no botão buscar', () => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+    cy.get('button').click();
+  });
+});
+
+// ----------------- TESTE 3: DIGITANDO A PALAVRA POODLE NO CAMPO DE INPUT------------------
+describe('teste campo de input', () => {
+  it('deve digitar a palavra Poodle no campo de input', () => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+    cy.get('input').type('Poodle');
+  });
+});
+
+// ----------------- TESTE 4: SIMULANDO A JORNADA DO USUÁRIO------------------
+describe('simulação da jornada do usuario', () => {
+  it('deve abrir a pagina e verificar se o body esta aparecendo', () => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+    cy.get('body').should('be.visible');
+    cy.get('input').type('Poodle');
+    cy.wait(8000); // espera 4 segundos
+    cy.visit('http://127.0.0.1:5500/index.html');
+    cy.get('button').click();
+    cy.wait(10000); // espera 5 segundos
+  });
+});
+
